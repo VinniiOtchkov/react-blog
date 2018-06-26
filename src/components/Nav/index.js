67a19a6de0e9d29404403ecc73ffc6cf03702f17
vinniiotchkov <vinniiotchkov@gmail.com>
2018-06-25 23:38:31 -0700

    Configuring for github pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,7 +8,7 @@ const Nav = () => {
         return (
             <div>
                 <Navbar brand="Dauntless" onClick={(e)=> e.preventDefault()} right>
-                    <NavItem title="Home" onClick={(e) => goHome(e)}><NavLink activeStyle={{color: '#7A7265'}} exact to="/"><Icon>home</Icon></NavLink></NavItem>
+                    <NavItem title="Home"><NavLink activeStyle={{color: '#7A7265'}} exact to="/"><Icon>home</Icon></NavLink></NavItem>
                     <NavItem title="Add Post"><NavLink activeStyle={{color: '#7A7265'}} exact to="/post/new"><Icon>add</Icon></NavLink></NavItem>
                 </Navbar>
 
